530d5c340ed1209c9f59de1a57467ef438d4002c
Crank-Git <Crank-Git@users.noreply.github.com>
2026-03-11 19:33:04 -0400

    demo fixes

--- packages/demo/web/vite.config.ts
+++ packages/demo/web/vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dir = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  root: __dir,
   server: {
     port: 3300,
     proxy: {
